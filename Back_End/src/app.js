9a4c98db8d0de75033ef2ef5839b655f59499b88
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-11-16 13:58:20 +0100

    ✨ SP03 > creation des routes utilisateurs

--- Back_End/src/app.js
+++ Back_End/src/app.js
@@ -4,12 +4,14 @@ const cors = require("cors");
 
 const app = express();
 
-const authRoutes = require("../src/routes/auth");
+const authRoutes = require("./routes/auth");
+const authUsers = require("./routes/users");
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth/", authRoutes);
+app.use("/api/users/", authUsers);
 
 module.exports = app;
